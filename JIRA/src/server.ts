765544dd1bd83c79e469e9caa548d25214239662
Sambhav-3010 <sambhavmani.tripathi@gmail.com>
2026-02-07 22:13:03 +0530

    done

--- JIRA/src/server.ts
+++ JIRA/src/server.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import express from "express";
+import express, { Request, Response } from "express";
 import connectDB from "./config/db";
 import jiraRoutes from "./jira_routes";
 
@@ -8,6 +8,9 @@ connectDB();
 const app = express();
 
 app.use("/jira", jiraRoutes);
+app.get("/", (req: Request, res: Response) => {
+  res.send("JIRA API running");
+});
 
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
